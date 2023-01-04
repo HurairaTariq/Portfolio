526e5cb44b515aafc350e4146b26f77e36acf1b7
Huraira Tariq <hurairatariq143@gmail.com>
2023-01-04 17:54:18 +0500

    first commit

---
+++ pages/about.js
@@ -0,0 +1,25 @@
+import Layout from '../components/layout'
+import { motion } from 'framer-motion'
+
+export default function About() {
+  return (
+    <Layout>
+      <motion.div
+        key="about"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+        className="px-4"
+      >
+        <div className="mb-6 text-center text-gray-800 dark:text-white">
+          Hi, I'm Huraira! I am a Machine Learning Engineer at DiveDeepAI with a passion for developing AI algorithms that make a positive impact on peoples lives.
+        </div>
+        <div className="text-center text-gray-800 dark:text-white">
+          In addition to coding and learning new tech, I enjoy rock climbing, drinking coffee,
+          playing guitar, reading, and hanging with my family. If any of these things interest you too,
+          I'd love to chat!
+        </div>
+      </motion.div>
+    </Layout>
+  )
+}
